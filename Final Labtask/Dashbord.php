bd23cdef3f6bbcb550391e03687b3fe0be69c737
JadidRahman <jadidrahman@outlook.com>
2021-07-18 13:48:08 +0600

    lab task

---
+++ Final Labtask/Dashbord.php
@@ -0,0 +1,32 @@
+<?php
+	$title= "Dashbord";
+?>
+							<div id="sidebar" class="">
+                <ul>
+                  <li><a href="Dashboard.php">Dashbord</a></li>
+                </ul>
+								</div>
+              </td>
+
+              <td>
+                <table id="box" border="1" width ="100%" cellspacing="0">
+                  <tr >
+
+                    <td align = "center">
+                      <a href="addStudent.php"><img height="100px" weight="100px" src="../Resources/student.jpg" alt=""></a>
+                      <br>
+                      <a href="addStudent.php">Student</a>
+                    </td>
+                    <td align = "center">
+                      <a href="department.php"><img height="100px" weight="100px" src="../Resources/department.jpg" alt=""></a>
+                      <br>
+                      <a href="department.php">Departments</a>
+                    </td>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+
+<?php include('footer.php'); ?>
